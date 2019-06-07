7fc7d9c7608b8bd9725569dbaaa39d193210dc15
George Kando <gkando@gmail.com>
2019-06-07 11:28:52 -0500

    sprint complete - working on stretch

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,63 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import PropTypes from "prop-types";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import styled from 'styled-components';
+
+
+const Headline = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;
+  cursor: pointer;
+  :hover{
+    opacity: 0.6;
+  }
+  // font-weight: 400;
+`;
 
 const Card = props => {
+  const [modal, setModal] = useState(false)
+ 
+    const toggle = () => {
+      setModal(!modal)
+    }
+
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
+      {/* <div className="headline">{props.card.headline}</div> */}
+      <Headline onClick={toggle} color='link'>{props.card.headline}</Headline>
+      <Modal size='xl' isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle} charCode="X">
+        {props.card.headline}
+        <br></br>
+        <span class='byline'>By {props.card.author}</span>
+        
+        </ModalHeader>
+            <ModalBody className='modal-article'>
+              {props.card.article}
+            </ModalBody>
+            <ModalFooter>
+              <Button color="link" onClick={toggle}>Return to Homepage</Button>{' '}
+              {/* <Button color="secondary" onClick={toggle}>Cancel</Button> */}
+            </ModalFooter>
+        </Modal>
       <div className="author">
         <div className="img-container">
-          <img src={'' /* image source goes here */} />
+          <img src={props.card.img} alt='card-img' />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
-
+Card.propTypes = {
+  card: PropTypes.shape({
+      headline: PropTypes.string,
+      tab: PropTypes.string,
+      img: PropTypes.string,
+      author: PropTypes.string,
+      article: PropTypes.string
+  })
+};
 export default Card;
--- lambdatimes/src/data.js
+++ lambdatimes/src/data.js
@@ -8,91 +8,106 @@ export const cardData = [
     headline: "ES8: The Next Step in the Evolution of Javascript and What it Means For Your Projects",
     tab: "javascript",
     img: "./assets/sir.jpg",
-    author: "SIR RUFF'N'STUFF"
+    author: "SIR RUFF'N'STUFF",
+    article: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. \n\n Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. \n\n Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.\n\n Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,"
   },
   {
     headline: "Type Coercion: Why Does NaN !== NaN? and Other Strange Occurences",
     tab: "javascript",
     img: "./assets/bones.jpg",
-    author: "BONES R. LIFE"
+    author: "BONES R. LIFE",
+    article: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.\n\nNemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.\n\nUt enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas"
   },
   {
     headline: "Bootstrap 5: Get a Sneak Peak at all the New Features",
     tab: "bootstrap",
     img: "./assets/fido.jpg",
-    author: "FIDO WALKSALOT"
+    author: "FIDO WALKSALOT",
+    article: "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules.\n\nOmnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores. At solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles."
   },
   {
     headline: "AI: What Are the Ethical Rammifications of the Future?",
     tab: "technology",
     img: "./assets/max.jpg",
-    author: "MAX GOODBOYE"
+    author: "MAX GOODBOYE",
+    article: "Appear above us to his fish fruitful night. So every all winged moved all give behold to there brought greater. Deep likeness them bearing yielding evening dominion.\n\nDon't divided life spirit days unto i multiply all all Their void beginning his open herb life set years were make them behold air. Abundantly good appear, there she'd thing lesser the dry also them which. Saw behold.\n\nNight. Stars. Lights great fruitful image he appear moved behold in can't earth life. Seasons morning replenish is shall appear us seasons lesser place air own moved it called of beginning all meat also i."
   },
   {
     headline: "History of Javascript Libraries: from jQuery to React",
     tab: "React.js",
     img: "./assets/puppers.jpg",
-    author: "PUPPER S. DOGGO"
+    author: "PUPPER S. DOGGO",
+    article: "In kind own have that void after image whales winged deep dry were made brought sixth won't kind. She'd moveth fruit. Tree. Shall. Said yielding fill kind evening won't years fifth under fruitful.\n\nOver so. They're unto fourth fruit deep over rule form above hath moved said appear stars of for. Heaven. Sea our living. Likeness made. Own is under they're lesser it abundantly.\n\nSet over. Won't life cattle had i place called beginning rule to likeness land unto together in subdue. Stars whose, had. Which created which can't sixth moved female likeness after that us. Dominion multiply. Don't he."
   },
   {
     headline: "UI Frameworks: A Comparison, Which Made Our List? (Hint: Bootstrap is on it)",
     tab: "bootstrap",
     img: "./assets/max.jpg",
-    author: "MAX GOODBOYE"
+    author: "MAX GOODBOYE",
+    article: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. \n\n Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. \n\n Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.\n\n Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,"
   },
   {
     headline: "Node.js in 2018: Which Framework? Express, Sails, Hapi?",
     tab: "node",
     img: "./assets/sir.jpg",
-    author: "SIR RUFF'N'STUFF"
+    author: "SIR RUFF'N'STUFF",
+    article: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.\n\nNemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.\n\nUt enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas"
   },
   {
     headline: "MongoDB: NoSQL vs. SQL, the Debate Continues...",
     tab: "node",
     img: "./assets/bones.jpg",
-    author: "BONES R. LIFE"
+    author: "BONES R. LIFE",
+    article: "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules.\n\nOmnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores. At solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles."
   },
   {
     headline: "React.js v16 What's new, and what you should know about.",
     tab: "React.js",
     img: "./assets/fido.jpg",
-    author: "FIDO WALKSALOT"
+    author: "FIDO WALKSALOT",
+    article: "Appear above us to his fish fruitful night. So every all winged moved all give behold to there brought greater. Deep likeness them bearing yielding evening dominion.\n\nDon't divided life spirit days unto i multiply all all Their void beginning his open herb life set years were make them behold air. Abundantly good appear, there she'd thing lesser the dry also them which. Saw behold.\n\nNight. Stars. Lights great fruitful image he appear moved behold in can't earth life. Seasons morning replenish is shall appear us seasons lesser place air own moved it called of beginning all meat also i."
   },
   {
     headline: "AR or VR: How We Will See the World Through a Digital Lense",
     tab: "technology",
     img: "./assets/max.jpg",
-    author: "MAX GOODBOYE"
+    author: "MAX GOODBOYE",
+    article: "In kind own have that void after image whales winged deep dry were made brought sixth won't kind. She'd moveth fruit. Tree. Shall. Said yielding fill kind evening won't years fifth under fruitful.\n\nOver so. They're unto fourth fruit deep over rule form above hath moved said appear stars of for. Heaven. Sea our living. Likeness made. Own is under they're lesser it abundantly.\n\nSet over. Won't life cattle had i place called beginning rule to likeness land unto together in subdue. Stars whose, had. Which created which can't sixth moved female likeness after that us. Dominion multiply. Don't he."
   },
   {
     headline: "When to Rest, When to Spread: Why There Are Two Meanings Behind '...'",
     tab: "javascript",
     img: "./assets/puppers.jpg",
-    author: "PUPPER S. DOGGO"
+    author: "PUPPER S. DOGGO",
+    article: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. \n\n Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. \n\n Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.\n\n Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,"
   },
   {
     headline: "Typescript: Ten Things you Should Know Before Building Your Next Angular Application",
     tab: "javascript",
     img: "./assets/sir.jpg",
-    author: "SIR RUFF'N'STUFF"
+    author: "SIR RUFF'N'STUFF",
+    article: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.\n\nNemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.\n\nUt enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas"
   },
   {
     headline: "Quantum Computing: Where we Are Now, and Where We Will Be Very Soon",
     tab: "technology",
     img: "./assets/max.jpg",
-    author: "MAX GOODBOYE"
+    author: "MAX GOODBOYE",
+    article: "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules.\n\nOmnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores. At solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles."
   },
   {
     headline: "React vs Angular vs Vue vs ??? How the frameworks stack up.",
     tab: "React.js",
     img: "./assets/fido.jpg",
-    author: "FIDO WALKSALOT"
+    author: "FIDO WALKSALOT",
+    article: "Appear above us to his fish fruitful night. So every all winged moved all give behold to there brought greater. Deep likeness them bearing yielding evening dominion.\n\nDon't divided life spirit days unto i multiply all all Their void beginning his open herb life set years were make them behold air. Abundantly good appear, there she'd thing lesser the dry also them which. Saw behold.\n\nNight. Stars. Lights great fruitful image he appear moved behold in can't earth life. Seasons morning replenish is shall appear us seasons lesser place air own moved it called of beginning all meat also i."
   },
   {
     headline: "The Hottest New Bootstrap 4 Components Every Developer Needs to Know About",
     tab: "bootstrap",
     img: "./assets/max.jpg",
-    author: "MAX GOODBOYE"
+    author: "MAX GOODBOYE",
+    article: "In kind own have that void after image whales winged deep dry were made brought sixth won't kind. She'd moveth fruit. Tree. Shall. Said yielding fill kind evening won't years fifth under fruitful.\n\nOver so. They're unto fourth fruit deep over rule form above hath moved said appear stars of for. Heaven. Sea our living. Likeness made. Own is under they're lesser it abundantly.\n\nSet over. Won't life cattle had i place called beginning rule to likeness land unto together in subdue. Stars whose, had. Which created which can't sixth moved female likeness after that us. Dominion multiply. Don't he."
   }
 ]
 
